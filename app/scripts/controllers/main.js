55b40011ca54bb35ea04b3aafb077d03be0e870e
Kell, Sean <skell@ea.com>
2015-09-14 14:31:47 -0400

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geartrackerClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the geartrackerClientApp
+ */
+angular.module('geartrackerClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
